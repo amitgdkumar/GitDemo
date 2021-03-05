253c6734542019b25b252d54118a8a38c91aaf7b
Amit Kumar Srivastava <amitgdkumar@gmail.com>
2021-03-05 12:33:17 +0530

    Second Commit

--- demo.java
+++ demo.java
@@ -6,5 +6,11 @@ public class demo {
 		// Write Selenium code here.
 
 	}
+	
+	public static void main1(String[] args) {
+		// TODO Auto-generated method stub
+		// Write Selenium code here.
+
+	}
 
 }
